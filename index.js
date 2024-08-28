18185fe7a3d0687c5dfaebc51ee344bf3599175c
djokhar05 <dedon4@live.com>
2024-08-28 14:46:47 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const setupPingers = require("./pingers");
+setupPingers();
+
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const port = process.env.PORT || 5500;
+const server = app.listen(port, () => {
+  let serverAddress = server.address().address;
+  let host = serverAddress === "::" ? "localhost" : serverAddress;
+  console.log(`Server listening at http://${host}:${port}`);
+});
+
+module.exports = app;
