1f52223489c74b10f10b33707e32970f824754d3
djokhar05 <dedon4@live.com>
2024-08-29 12:12:21 +0100

    Testing on render again - 4

---
+++ src/pingers.js
@@ -0,0 +1,34 @@
+const cron = require("node-cron");
+const axios = require("axios");
+
+const servers_to_ping = [
+  "https://roundtripzdriverserver.onrender.com",
+  "https://roundtripzserver.onrender.com",
+  "https://roundtripz-socket.onrender.com",
+];
+
+const pingServers = async () => {
+  try {
+    await Promise.all(
+      servers_to_ping.map(async (server, index) => {
+        console.log(`Pinging ${server} ...`);
+        const response = await axios.get(server);
+        console.log(`${index + 1}. ${response.data}`);
+      })
+    );
+    console.log("Pinging alls Servers Completed");
+  } catch (error) {
+    // console.log(error);
+    console.log("An error occured while pinging the server" + server);
+  }
+};
+
+pingServers();
+
+// console.log("Cron Pingers Being Setup");
+// module.exports = () => {
+//   cron.schedule("*/5 * * * *", async () => {
+//     console.log("Pinging Servers ...");
+//     await pingServers();
+//   });
+// };
